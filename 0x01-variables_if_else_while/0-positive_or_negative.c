a8f421ce945f709b04aad84b334e2401cb5a9f0d
WalkerStanislas <walkercompaore972@gmail.com>
2023-03-18 09:12:59 -0700

    question0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/**
+ * main - Positive anything is better than negative nothing 
+ * return: always 0.
+ */
 int main(void)
 {
 	int n;
