5f5e31231189f24dc7e445ad0d68bd257cc01186
WalkerStanislas <walkercompaore972@gmail.com>
2023-03-19 16:04:24 -0700

    question6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints alphabetics
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
