11b2b6fb64f0b5705a96bf5d3adf598557126305
WalkerStanislas <walkercompaore972@gmail.com>
2023-03-19 16:41:20 -0700

    question6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib>
+#include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
 /**
- * main - Prints alphabetics
- * Return: Always (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -13,7 +15,7 @@ int main(void)
 
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < '10'; i++)
 	{
 		putchar(i);
 	}
