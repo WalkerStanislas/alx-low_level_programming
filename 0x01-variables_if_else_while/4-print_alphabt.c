3e25009552856bc33874c24bb2a2c205ef7e376a
WalkerStanislas <walkercompaore972@gmail.com>
2023-03-19 15:41:53 -0700

    question4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints alphabetics
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'e' or alpha == 'q')
+		{
+			continue;
+		}
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+
+}
