d24658d3a1bbe6e1f4bdc22be110d5a6d767d000
WalkerStanislas <walkercompaore972@gmail.com>
2023-03-19 15:29:43 -0700

    question3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints alphabetics
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+
+}
