fddd1db69982c1e032f711926d8be6ff5f1dabf6
WalkerStanislas <walkercompaore972@gmail.com>
2023-03-19 15:53:06 -0700

    question5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints numbers
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+
+}
