6b28af20c39fff7d609d73e8378a0f764d019291
WalkerStanislas <walkercompaore972@gmail.com>
2023-03-19 17:48:00 -0700

    question9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,11 @@ int main (void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == '9')
-		{
-			break;
-		}
-		else
-		{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
