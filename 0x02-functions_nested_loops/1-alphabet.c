2cc7772d2ce1c0a74cb2f7f4457274b22209a24e
WalkerStanislas <walkercompaore972@gmail.com>
2023-03-21 13:16:06 -0700

    question1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,10 @@
  */
 void print_alphabet(void)
 {
-
-        char a;
-
-        for (a = 'a'; a <= 'z'; a++)
-        {
-                _putchar(a);
-        }
-        _putchar('\n');
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
 }
